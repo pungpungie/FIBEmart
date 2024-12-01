1e8005ebe908ee59f4b4c6de601c2c561f9def5d
pungpungie <541241053@student.smktelkom-pwt.sch.id>
2024-12-01 22:33:09 +0700

    Add files via upload

---
+++ script-login.js
@@ -0,0 +1,23 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === 'user' && password === 'pass') {
+        document.getElementById('message').textContent = 'Login successful!';
+    } else {
+        document.getElementById('message').textContent = 'Invalid username or password.';
+    }
+});
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
